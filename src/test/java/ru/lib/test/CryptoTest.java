2c57634d16be7111ea61e50c59a22e4351e31b20
Andranik <andranik_ta@mail.ru>
2018-10-27 12:56:18 +0300

    Пробуем в деле javax.crypto

---
+++ src/test/java/ru/lib/test/CryptoTest.java
@@ -0,0 +1,73 @@
+package ru.lib.test;
+
+import org.junit.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+
+public class CryptoTest {
+
+    private static final String testMsg = "Hello. It's test message!";
+
+    @Test
+    public void test() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance("RSA");
+
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        Key publicKey = keyPair.getPublic();
+        Key privateKey = keyPair.getPrivate();
+        System.out.println("Start message: " + testMsg);
+
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptBytes = cipher.doFinal(testMsg.getBytes());
+        System.out.println("EnCrypt message: " + new String(encryptBytes));
+
+        Cipher decryptCipher = Cipher.getInstance("RSA");
+        decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptBytes = decryptCipher.doFinal(encryptBytes);
+        System.out.println("DeCrypt message: " + new String(decryptBytes));
+    }
+}
+
+/*
+        1.Generate Symmetric Key (AES with 128 bits)
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(128); // The AES key size in number of bits
+        SecretKey secKey = generator.generateKey();
+
+        2.Encrypt plain text using AES
+        String plainText = "Please encrypt me urgently..."
+        Cipher aesCipher = Cipher.getInstance("AES");
+        aesCipher.init(Cipher.ENCRYPT_MODE, secKey);
+        byte[] byteCipherText = aesCipher.doFinal(plainText.getBytes());
+
+        3.Encrypt the key using RSA public key
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+        KeyPair keyPair = kpg.generateKeyPair();
+
+        PublicKey puKey = keyPair.getPublic();
+        PrivateKey prKey = keyPair.getPrivate();
+
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.PUBLIC_KEY, puKey);
+        byte[] encryptedKey = cipher.doFinal(secKey.getEncoded());
+
+        4.Send encrypted data (byteCipherText) + encrypted AES Key (encryptedKey)
+
+        5.On the client side, decrypt symmetric key using RSA private key
+        cipher.init(Cipher.PRIVATE_KEY, prKey);
+        byte[] decryptedKey = cipher.doFinal(encryptedKey);
+
+        6. Decrypt the cipher using decrypted symmetric key
+        //Convert bytes to AES SecertKey
+        SecretKey originalKey = new SecretKeySpec(decryptedKey , 0, decryptedKey .length, "AES");
+        Cipher aesCipher = Cipher.getInstance("AES");
+        aesCipher.init(Cipher.DECRYPT_MODE, originalKey);
+        byte[] bytePlainText = aesCipher.doFinal(byteCipherText);
+        String plainText = new String(bytePlainText);
+*/
--- src/test/java/ru/lib/test/SettingKeeperTest.java
+++ src/test/java/ru/lib/test/SettingKeeperTest.java
@@ -7,18 +7,18 @@ import ru.lib.impl.SettingKeeper;
 
 public class SettingKeeperTest {
 
-    private static final String test_filepath1 = "test-config.properties";
-    private static final String bad_test_filepath = "unknown.properties";
+    private static final String testFilepath = "test-config.properties";
+    private static final String badTestFilepath = "unknown.properties";
 
     @Test
     public void test1() {
-        ISettingKeeper settingKeeper =  new SettingKeeper(test_filepath1);
+        ISettingKeeper settingKeeper = new SettingKeeper(testFilepath);
         Assert.assertFalse(settingKeeper.getSettings().isEmpty());
     }
 
     @Test
     public void test2() {
-        ISettingKeeper settingKeeper =  new SettingKeeper(bad_test_filepath);
+        ISettingKeeper settingKeeper = new SettingKeeper(badTestFilepath);
         Assert.assertTrue(settingKeeper.getSettings().isEmpty());
     }
 
@@ -27,7 +27,7 @@ public class SettingKeeperTest {
         String code = "test_code";
         String value = "test_value";
 
-        ISettingKeeper settingKeeper =  new SettingKeeper();
+        ISettingKeeper settingKeeper = new SettingKeeper();
         settingKeeper.setSetting(code, value);
         Assert.assertEquals(value, settingKeeper.getSetting(code));
     }
