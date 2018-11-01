47b3750aa67c2565cc0288d3899a2b6f4be0c94c
park <OoopsIamSorry@gmail.com>
2018-11-01 22:01:11 +0300

    Merge remote-tracking branch 'remotes/origin/master' into forms
    # Conflicts:
    #	desktop/src/main/java/ru/tcezar/blockchain/App.java

--- desktop/src/main/java/ru/tcezar/blockchain/forms/ApplicationForm.java
+++ desktop/src/main/java/ru/tcezar/blockchain/forms/ApplicationForm.java
@@ -1,5 +1,6 @@
 package ru.tcezar.blockchain.forms;
 
+import ru.tcezar.blockchain.Member;
 import ru.tcezar.blockchain.api.IMember;
 import ru.tcezar.crypto.api.ICryptoUtils;
 import ru.tcezar.crypto.impl.CryptoUtils;
@@ -19,12 +20,12 @@ public class ApplicationForm extends JFrame {
 
     private SendFileForm sendFileForm;
     private ListOfMembers listOfMembers;
-    private IMember member;
+    private Member member;
 
-    public ApplicationForm(IMember member) throws GeneralSecurityException {
+    public ApplicationForm(Member member) throws GeneralSecurityException {
         this.member = member;
 
-        listOfMembers = new ListOfMembers();
+        listOfMembers = new ListOfMembers(member);
 
         //Свойства формы по-умолчанию
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -32,7 +33,9 @@ public class ApplicationForm extends JFrame {
 
         add(mainPanel);
 
-        tabbedPane1.setComponentAt(0, listOfMembers.getMembers());
+        JLabel jLabel = new JLabel(listOfMembers.getMembers());
+
+        tabbedPane1.setComponentAt(0, jLabel);
 
         ICryptoUtils cryptoUtils = new CryptoUtils();
 
@@ -55,9 +58,6 @@ public class ApplicationForm extends JFrame {
                             }
                         });
                         tabbedPane1.setComponentAt(1, jButton);
-                        break;
-                    case 0:
-                        tabbedPane1.setComponentAt(0, listOfMembers.getMembers());
                         break;
                 }
 
