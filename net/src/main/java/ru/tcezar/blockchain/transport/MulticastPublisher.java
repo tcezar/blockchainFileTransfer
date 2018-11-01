f361c7b59542fbf3cb0ce76ecbc0a8e5c0db89fd
BatMich <m1h-bat@yandex.ru>
2018-11-01 15:39:41 +0300

    адаптация мультикаст отправителя к IMessageData

--- net/src/main/java/ru/tcezar/blockchain/transport/MulticastPublisher.java
+++ net/src/main/java/ru/tcezar/blockchain/transport/MulticastPublisher.java
@@ -6,17 +6,34 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 
 public class MulticastPublisher {
     private DatagramSocket socket;
     private InetAddress group;
+    private int port;
+
     private byte[] buf;
 
+    protected MulticastPublisher(MulticastSocket socket, InetAddress group, int port) throws IOException {
+        init(socket, group, port);
+    }
+
+    public MulticastPublisher(String socketAddr, int socketPort) throws IOException {
+        DatagramSocket datagramSocket = new DatagramSocket();
+        InetAddress group = InetAddress.getByName(socketAddr);
+        init(datagramSocket, group, socketPort);
+    }
+
+    private void init(DatagramSocket socket, InetAddress group, int port) {
+        this.socket = socket;
+        this.group = group;
+        this.port = port;
+    }
+
     public void multicast(IMessageData multicastMessage) throws IOException {
-        socket = new DatagramSocket();
-        group = InetAddress.getByName("230.0.0.0");
         buf = SerializationUtils.serializeObject(multicastMessage);
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, group, 4446);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, group, port);
         socket.send(packet);
         socket.close();
     }
