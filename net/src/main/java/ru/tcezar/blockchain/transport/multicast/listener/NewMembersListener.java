3b995b1b4b440d05fc14a334596aca525a300fcf
BatMich <m1h-bat@yandex.ru>
2018-11-01 15:50:54 +0300

    заготовка для слушателей multicast

---
+++ net/src/main/java/ru/tcezar/blockchain/transport/multicast/listener/NewMembersListener.java
@@ -0,0 +1,21 @@
+package ru.tcezar.blockchain.transport.multicast.listener;
+
+import ru.tcezar.blockchain.api.IMember;
+import ru.tcezar.blockchain.transport.api.IListenerNewMembers;
+
+import java.util.List;
+
+/**
+ * Created by Michael on 01.11.2018.
+ */
+public class NewMembersListener implements IListenerNewMembers {
+    @Override
+    public void setMembers(List<IMember> members) {
+        
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
