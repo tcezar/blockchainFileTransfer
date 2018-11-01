4a4d95b9f1fd8c979df3d6357b6ae6742c07dfc4
nik0@mail.ru <Diablo023472>
2018-11-01 20:43:16 +0300

    uid

--- net/src/main/java/ru/tcezar/blockchain/transport/listener/multicast/NewMembersListener.java
+++ net/src/main/java/ru/tcezar/blockchain/transport/listener/multicast/NewMembersListener.java
@@ -3,6 +3,7 @@ package ru.tcezar.blockchain.transport.listener.multicast;
 import ru.tcezar.blockchain.api.IBlockChain;
 import ru.tcezar.blockchain.api.IMember;
 import ru.tcezar.blockchain.api.IMessage;
+import ru.tcezar.blockchain.api.UID;
 import ru.tcezar.blockchain.transport.udp.multicast.AbstractMulticastReceiver;
 import ru.tcezar.blockchain.transport.api.INewMembersListener;
 import ru.tcezar.blockchain.transport.messages.Message;
@@ -11,14 +12,16 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Michael on 01.11.2018.
  */
 public class NewMembersListener extends AbstractMulticastReceiver implements INewMembersListener {
 
-    private List<IMember> addressBook = new ArrayList<>();
+    private Set<UID> addressBook = new HashSet<>();
     private IBlockChain blockChain = null;
 
     protected NewMembersListener(MulticastSocket socket, InetAddress group) throws IOException {
@@ -35,7 +38,7 @@ public class NewMembersListener extends AbstractMulticastReceiver implements INe
     }
 
     @Override
-    public void setMembers(List<IMember> members) {
+    public void setMembers(Set<UID> members) {
         this.addressBook = members;
     }
 
