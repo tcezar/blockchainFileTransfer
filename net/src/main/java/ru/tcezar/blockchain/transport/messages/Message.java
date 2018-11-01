bbd0a1c20a87794a7bd762e769c11a35d6d75748
BatMich <m1h-bat@yandex.ru>
2018-11-01 19:08:47 +0300

    снова чистка

--- net/src/main/java/ru/tcezar/blockchain/transport/messages/Message.java
+++ net/src/main/java/ru/tcezar/blockchain/transport/messages/Message.java
@@ -5,8 +5,6 @@ import ru.tcezar.blockchain.api.IMessage;
 
 import java.io.Serializable;
 
-import static ru.tcezar.blockchain.transport.protocols.NewMembersMessageCommand.HELLO;
-
 /**
  * Created by Michael on 01.11.2018.
  */
@@ -20,29 +18,6 @@ public class Message<T extends Serializable> implements IMessage {
         this.sender = sender;
         this.messageData = data;
     }
-
-    /**
-     * Создание сообщения HELLO
-     *
-     * @param recipient Получатель сообщения
-     * @param sender    Отправитель сообщения
-     * @return Сообщение HELLO
-     */
-    public static Message createHelloMessage(IMember recipient, IMember sender) {
-        return new Message(recipient, sender, HELLO);
-    }
-
-    /**
-     * Создание ответного сообщения с текущим размером цепочик
-     *
-     * @param recipient Получатель сообщения
-     * @param sender    Отправитель сообщения
-     * @return Сообщение HELLO_ANSWER
-     */
-    public static Message createHelloAnswerMessage(IMember recipient, IMember sender, Integer blockChainSize) {
-        return new Message(recipient, sender, blockChainSize);
-    }
-
 
     @Override
     public IMember getRecipient() {
