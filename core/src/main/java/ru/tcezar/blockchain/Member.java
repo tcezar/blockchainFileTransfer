c61549c70da6c2cda423750d2d1acc53ef05c67b
Andranik <andranik_ta@mail.ru>
2018-11-01 20:23:17 +0300

    ключи

--- core/src/main/java/ru/tcezar/blockchain/Member.java
+++ core/src/main/java/ru/tcezar/blockchain/Member.java
@@ -3,12 +3,15 @@ package ru.tcezar.blockchain;
 import ru.tcezar.blockchain.api.IBlockChain;
 import ru.tcezar.blockchain.api.IMember;
 import ru.tcezar.blockchain.transport.api.*;
+import ru.tcezar.config.ConfigKeeper;
 import ru.tcezar.crypto.api.ICryptoUtils;
 import ru.tcezar.crypto.api.IPairKeys;
 import ru.tcezar.crypto.impl.CryptoUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,11 +41,25 @@ public final class Member implements IMember {
     final private List<IMember> members;
     final private InetAddress localAdress;
 
-    public Member() throws GeneralSecurityException, UnknownHostException {
+    public Member() throws GeneralSecurityException, IOException {
         ICryptoUtils cryptoUtils = new CryptoUtils();
-        keys = cryptoUtils.generateKeys();
+        if(ConfigKeeper.checkKeysFilepathConsist()) {
+            keys = cryptoUtils.getKeysFromFiles(
+                    Paths.get(String.valueOf(ConfigKeeper.getConfig(ConfigKeeper.publicKeyCode))),
+                    Paths.get(String.valueOf(ConfigKeeper.getConfig(ConfigKeeper.privateKeyCode))));
+        } else {
+            keys = cryptoUtils.generateKeys();
+            ConfigKeeper.setConfig(ConfigKeeper.publicKeyCode, ConfigKeeper.configDir + File.pathSeparator +
+                    ConfigKeeper.publicKeyCode + keys.getPublicKey().hashCode());
+            ConfigKeeper.setConfig(ConfigKeeper.privateKeyCode, ConfigKeeper.configDir + File.pathSeparator +
+                    ConfigKeeper.privateKeyCode + keys.getPrivateKey().hashCode());
+            cryptoUtils.saveKeysToFiles(keys,
+                    Paths.get(String.valueOf(ConfigKeeper.getConfig(ConfigKeeper.publicKeyCode))),
+                    Paths.get(String.valueOf(ConfigKeeper.getConfig(ConfigKeeper.privateKeyCode))));
+            ConfigKeeper.saveConfigsToFile();
+        }
         blockChain = new BlockChain();
-        id = String.valueOf(keys.getPublicKey().getEncoded());
+        id = String.valueOf(keys.getPublicKey().hashCode());
         listeners = Executors.newFixedThreadPool(3);
         singleTasks = Executors.newSingleThreadExecutor();
         singleFileTransfers = Executors.newSingleThreadExecutor();
