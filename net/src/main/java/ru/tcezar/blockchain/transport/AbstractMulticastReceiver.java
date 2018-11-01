e373829a63b3a52d994bdf34fbb07dd27b1f028a
BatMich <m1h-bat@yandex.ru>
2018-11-01 14:11:15 +0300

    абстрактный слушатель Multicast

---
+++ net/src/main/java/ru/tcezar/blockchain/transport/AbstractMulticastReceiver.java
@@ -0,0 +1,66 @@
+package ru.tcezar.blockchain.transport;
+
+import ru.tcezar.blockchain.api.IMessageData;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public abstract class AbstractMulticastReceiver extends Thread {
+    protected MulticastSocket socket = null;
+    protected InetAddress group;
+
+    protected byte[] buf = new byte[256];
+
+    protected AbstractMulticastReceiver(MulticastSocket socket, InetAddress group) throws IOException {
+        init(socket, group);
+    }
+
+    protected AbstractMulticastReceiver(String socketAddr, int socketPort) throws IOException {
+        MulticastSocket multicastSocket = new MulticastSocket(socketPort);
+        InetAddress group = InetAddress.getByName(socketAddr);
+        init(multicastSocket, group);
+    }
+
+    private void init(MulticastSocket socket, InetAddress group) throws IOException {
+        this.socket = socket;
+        this.socket.joinGroup(group);
+        this.group = group;
+    }
+
+    /**
+     * Обработчик сообщения
+     *
+     * @param messageData Данные в сообщении
+     * @return True, если необходимо перестать слушать
+     */
+    protected abstract boolean processMessage(IMessageData messageData);
+
+    public void run() {
+        try {
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                try {
+                    IMessageData recivedMessage = SerializationUtils.getData(packet.getData());
+                    if (processMessage(recivedMessage)) {
+                        break;
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                String received = new String(
+                        packet.getData(), 0, packet.getLength());
+                if ("end".equals(received)) {
+                    break;
+                }
+            }
+            socket.leaveGroup(group);
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
