eb30cd982afe54fa924d002be7235a3b4ac560fb
BatMich <m1h-bat@yandex.ru>
2018-11-01 13:57:12 +0300

    Адаптировал MulticastPublisher для приема IMessageData

--- net/src/main/java/ru/tcezar/blockchain/transport/MulticastPublisher.java
+++ net/src/main/java/ru/tcezar/blockchain/transport/MulticastPublisher.java
@@ -1,5 +1,7 @@
 package ru.tcezar.blockchain.transport;
 
+import ru.tcezar.blockchain.api.IMessageData;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -10,14 +12,11 @@ public class MulticastPublisher {
     private InetAddress group;
     private byte[] buf;
 
-    public void multicast(
-            String multicastMessage) throws IOException {
+    public void multicast(IMessageData multicastMessage) throws IOException {
         socket = new DatagramSocket();
         group = InetAddress.getByName("230.0.0.0");
-        buf = multicastMessage.getBytes();
-
-        DatagramPacket packet
-                = new DatagramPacket(buf, buf.length, group, 4446);
+        buf = SerializationUtils.serializeObject(multicastMessage);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, group, 4446);
         socket.send(packet);
         socket.close();
     }
